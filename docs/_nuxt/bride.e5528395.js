2024b087ad0bf8e009d68a15d9751a21b26d0d4e
bwsunkist <54732163+bwsunkist@users.noreply.github.com>
2024-03-15 14:47:43 +0900

    デプロイ

---
+++ docs/_nuxt/bride.e5528395.js
@@ -0,0 +1,1 @@
+import{d as e,m as t,q as a,s as n,t as s}from"./entry.8230b07e.js";const o="bride",p=e({__name:"bride",setup(r){return(c,l)=>(t(),a("div",null,[n("h1",null,s(o))]))}});export{p as default};
